6bf9d495c0f5ea72258743694995f9000d01ceac
Abinaya5829 <abinayaa5829@gmail.com>
2025-06-04 11:37:16 +0530

    Added server.js file

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const http = require('http');
+const fs = reqiure('fs');
 
+const backend = https.createServer((req,res)=>{
+    if(req.method==='GET' && req.url==='/kitkat'){
+        res.statusCode=200;
+        res.seetHeader('Content-Type','text/plain');
+        res.end("kitkat");
+    }
+    else if(req.method==='GET' && req.url==='/munch'){
+        const readFile = ()=>{
+            return fetch.readFileSync('data.txt','utf-8');
+        }
+        const data = readFile();
+        res.end(data);
+
+        }
+    }
+);
+backend.listen(5000,()=>{
+    console.log("Server Running")
+});
